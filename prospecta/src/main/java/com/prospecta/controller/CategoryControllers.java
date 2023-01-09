8d7d2f799505ba46ffffac7a4e1f898f4db62fe3
VaibhavBedarkar <vaibhav.bedarkar98@gmail.com>
2023-01-09 15:42:59 +0530

    get controller

--- prospecta/src/main/java/com/prospecta/controller/CategoryControllers.java
+++ prospecta/src/main/java/com/prospecta/controller/CategoryControllers.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.prospecta.DTO.CustomEntryDTO;
+import com.prospecta.exceptions.EntryExceptions;
 import com.prospecta.model.APIResponse;
 import com.prospecta.model.Entry;
 
@@ -21,14 +22,23 @@ public class CategoryControllers {
 	RestTemplate restTemplate;
 	
 	@GetMapping("/entries/{category}")
-	public List<CustomEntryDTO> getEntriesByCategory(@PathVariable("category") String category){
+	public List<CustomEntryDTO> getEntriesByCategory(@PathVariable("category") String category) throws EntryExceptions{
 		APIResponse apiResponse = restTemplate.getForObject("https://api.publicapis.org/entries",APIResponse.class);
 		
 		APIResponse apiResp = restTemplate.getForObject("https://api.publicapis.org/entries", APIResponse.class);
 		List<Entry> allEntry = apiResp.getEntries();
-		List<CustomEntryDTO> categorywiseResponse = allEntry.stream().filter(e -> e.getCategory().equals(category)).map(e -> new CustomEntryDTO(e.getApi(), e.getDescription())).toList();
 		
-		return categorywiseResponse;
+		if(allEntry.size()==0) {
+			throw new EntryExceptions("Error connecting with API!");
+		}else {
+			List<CustomEntryDTO> categorywiseResponse = allEntry.stream().filter(e -> e.getCategory().equals(category)).map(e -> new CustomEntryDTO(e.getApi(), e.getDescription())).toList();
+		
+			if(categorywiseResponse.size()==0) {
+				throw new EntryExceptions("Check the category entered..!");
+			}else {
+				return categorywiseResponse;
+			}
+		}
 	}
 	
 	
---
+++ prospecta/src/main/java/com/prospecta/exceptions/EntryExceptions.java
@@ -0,0 +1,15 @@
+package com.prospecta.exceptions;
+
+public class EntryExceptions extends Exception {
+
+	public EntryExceptions() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EntryExceptions(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
